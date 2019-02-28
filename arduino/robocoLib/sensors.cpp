0c22075f1045fda5e889ba871de71106eeea74a0
leonardomello27 <31973297+leonardomello27@users.noreply.github.com>
2019-02-27 22:59:36 -0300

    Update sensors.cpp
    editado dia 27/02/2019

--- arduino/robocoLib/sensors.cpp
+++ arduino/robocoLib/sensors.cpp
@@ -1,28 +1,46 @@
 #include <Arduino.h>
 #include "sensors.h"
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+#include <SoftwareSerial.h>
 
-Motor::Motor(int8_t pin1, int8_t pin2, int8_t pinPwm){
-  this->pin1=pin1;
-  this->pin2=pin2;
-  this->pinPwm=pinPwm;
-  pinMode(pin1, OUTPUT);
-  pinMode(pin2, OUTPUT);
-  pinMode(pinPwm, OUTPUT);
+SoftwareSerial mySerial(19,18); // RX, TX
+byte cmd[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79};
+char response[9];
+int prevVal = LOW;
+long ppm = 0.0;
+
+Sensor::Sensor(int8_t pinLum) {
+  this->bpm = new Adafruit_BMP280();
+  this->pinA15 = pinLum;
+  pinMode(A15, OUTPUT);
   this->stop();
 }
 
-void Motor::stop(){
-  analogWrite(pinPwm, 0);
+float Sensor::getLuminosity(){
+  int valLum = analogRead(pinA15);
+  return(valLum);
 }
-void Motor::move(MotorDirection direction,unsigned char speed){
   
-  if(direction == CLOCKWISE){
-     digitalWrite(pin1, HIGH);
-     digitalWrite(pin2, LOW);   
-  }else{
-     digitalWrite(pin1, LOW);
-     digitalWrite(pin2, HIGH);
-  }
-  analogWrite(pinPwm, speed);
+float Sensor::getTemperature(){
+  return(bmp.readTemperature());
 }
 
+float Sensor::getAltitude(){
+  return(bmp.readAltitude(1013.25)); // this should be adjusted to your local forcase
+}
+
+float Sensor::getAtmosphericPressure(){
+ return(bmp.readPressure());
+}
+
+float sensor::getCO2ppm(){
+  mySerial.write(cmd,9);
+  mySerial.readBytes(response, 9);
+  int responseHigh = (int) response[2];
+  int responseLow = (int) response[3];
+  ppm = (256*responseHigh)+responseLow;
+  return (ppm);
+}
