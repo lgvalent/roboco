1fb8fd46d6936a133bdc565ff8ebb0805945604c
laismansano <32306960+laismansano@users.noreply.github.com>
2019-03-19 15:21:01 -0300

    Update workflow.cpp
    Atualização por Lais, Bruna e Larissa. 19/03/2019

--- arduino/robocoLib/workflow.cpp
+++ arduino/robocoLib/workflow.cpp
@@ -3,19 +3,40 @@
 #include "workflow.h"
 #include <SD.h>
 
+Workflow::Worflow (int8_t pinSD){
+  this->pinSD = pinSD;
+  this->latitude = latitude;
+  this->longitude = longitude;
+  this->collectCount = collectCount;
+  this->collectInterval = collectInterval;
+ 
+}
+
 Workstep* Workflow::getNextStep(){
-  file meuArquivo;
- meuArquivo = SD.open("roboco.txt", "r");
- if(meuArquivo == NULL)
+  Serial.begin(9600);
+  while (!Serial) {
+  }
+  
+  
+  
+  if ( ! SD. begin ( pinSD ) ) { //Como deixar a pinagem genérica?
+  lcd. claro ();
+  LCD ( 0 , 0 , "Inicialização falhou." );
+  while ( 1 ) ; 
+  } 
+ LCD ( 0 , 0 , "Inicialização feita." );
+  
+ File myFile;
+ myFile = SD.open("roboco.txt", "r");
+ if(myFile == NULL){
      return NULL;
+ }
  else
-     while( (ch=fgetc(arq))!= EOF )
+     while( (ch=fgetc(arq))!= EOF ) //leitura do arquivo caracter por caracter
      putchar(ch);
    
-  
-  
-  
  fclose(arq);
+
 }
   
 
