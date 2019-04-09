3b89acdd09849d155b397b5c04519f5074ba62ee
laismansano <32306960+laismansano@users.noreply.github.com>
2019-04-09 14:09:22 -0300

    Update workflow.cpp
    ajustes feitos por Bruna, Larissa, Lais e professor no dia 09/04.

--- arduino/robocoLib/workflow.cpp
+++ arduino/robocoLib/workflow.cpp
@@ -3,51 +3,55 @@
 #include "workflow.h"
 #include <SD.h>
 
+
+//passar todas as variaveis pro ingles.
+
 Workflow::Worflow (int8_t pinSD){
   this->pinSD = pinSD;
+  this->currentStopIndex = 0; //verificar o registrador do arduino em que passo parou (flash memory).
 }
 
 Workstep* Workflow::getNextStep(){
   Workstep* workstep = new Workstep;
-  this->currentStopIndex = -1;
   int linhaAtual = 0;
   int readLine = 0;
   Serial.begin(9600);
   while (!Serial) {
-  }
+  } // Limitar o laço para detectar erros no sensor.
   
   if ( ! SD. begin ( pinSD ) ) { //Como deixar a pinagem genérica?
-  lcd. claro ();
-  LCD ( 0 , 0 , "Inicialização falhou." );
-  while ( 1 ) ; 
-  } 
- LCD ( 0 , 0 , "Inicialização feita." );
-  
+
+ 
  File myFile;
  myFile = SD.open("roboco.txt", "r");
  if(myFile == NULL){
      return NULL;
  }
   
-  while ( ){//terminar
+  while ( linhaAtual != currentStopIndex){
+    while ( (ch=fgetc(arq))!= EOF) //verificar se fgetc funciona ou é myfile.read
+          if(ch=='/n'){ //verificar se é r ou n que vem por ultimo.
+            linhaAtual++;
+            break;
+          }
   }
-  
- else{
-   if (this->currentStopIndex == linhaAtual) {
-      this->latitude = latitude;
+  //ler a linha atual inteira.   se usar getvalue (precisa implementar) guardar no buffer antes.
+    
+      (this->currentStopIndex == linhaAtual) {
+      
+      this->latitude = getValue (buffer, ',', 0 ). toFloat (); //arrumar os outros igual a esse.
+      longitude = getValue (buffer, ',', 1 ). toFloat ();
+      collectInterval = getValue (buffer, ',', 5 ). toInt ();
+      collectCount = getValue (buffer, ',', 5). toInt ();
+      //fazer a leitura de cada caracter e guardar a junção dos caracteres em cada variavel, exemplo '100028749' = latitude.
       this->longitude = longitude;
       this->collectCount = collectCount;
       this->collectInterval = collectInterval;
-      latitude = getValue (buffer, ' , ' , 0 ). toFloat ();
-      longitude = getValue (buffer, ' , ' , 1 ). toFloat ();
-      collectInterval = getValue (buffer, ' , ' , 5 ). toInt ();
-      collectCount = getValue (buffer, ', ', 5). toInt ();
+        
    }
  readLine ++;
  }
-//while( (ch=fgetc(arq))!= EOF ) //leitura do arquivo caracter por caracter
-//putchar(ch);
-   
+
  fclose(arq);
 }
   
