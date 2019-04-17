cd1b9999a51aea860318caec31ad05a508802f00
laismansano <32306960+laismansano@users.noreply.github.com>
2019-04-16 21:20:26 -0300

    Update workflow.cpp
    Modificações feitas em relação a reunião do dia 16/04/19

--- arduino/robocoLib/workflow.cpp
+++ arduino/robocoLib/workflow.cpp
@@ -2,31 +2,30 @@
 #include <stdio.h>
 #include "workflow.h"
 #include <SD.h>
-
+#include <EEPROM.h>
+#define adress 0
 
 //passar todas as variaveis pro ingles.
 
 Workflow::Worflow (int8_t pinSD){
   this->pinSD = pinSD;
-  this->currentStopIndex = 0; //verificar o registrador do arduino em que passo parou (flash memory).
+  byte value;
+  byte currentStopIndex;
+  this->currentStopIndex = EEPROM.read(adress);
 }
 
 Workstep* Workflow::getNextStep(){
   Workstep* workstep = new Workstep;
   int linhaAtual = 0;
   int readLine = 0;
-  Serial.begin(9600);
-  while (!Serial) {
-  } // Limitar o laço para detectar erros no sensor.
   
-  if ( ! SD. begin ( pinSD ) ) { //Como deixar a pinagem genérica?
-
- 
- File myFile;
- myFile = SD.open("roboco.txt", "r");
- if(myFile == NULL){
-     return NULL;
- }
+  if ( ! SD. begin ( pinSD ) ) { 
+    File myFile;
+    myFile = SD.open("roboco.txt", "r");
+    if(myFile == NULL){
+      return NULL;
+     }
+  }
   
   while ( linhaAtual != currentStopIndex){
     while ( (ch=myFile.read(arq))!= EOF) 
@@ -38,19 +37,27 @@ Workstep* Workflow::getNextStep(){
   //ler a linha atual inteira.   se usar getvalue (precisa implementar) guardar no buffer antes.
     
       (this->currentStopIndex == linhaAtual) {
-      
       this->latitude = getValue (buffer, ',', 0 ). toFloat (); //arrumar os outros igual a esse.
-      longitude = getValue (buffer, ',', 1 ). toFloat ();
-      collectInterval = getValue (buffer, ',', 5 ). toInt ();
-      collectCount = getValue (buffer, ',', 5). toInt ();
+      this->longitude = getValue (buffer, ',', 1 ). toFloat ();
+      this->collectInterval = getValue (buffer, ',', 5 ). toInt ();
+      this->collectCount = getValue (buffer, ',', 5). toInt ();
       //fazer a leitura de cada caracter e guardar a junção dos caracteres em cada variavel, exemplo '100028749' = latitude.
       this->longitude = longitude;
       this->collectCount = collectCount;
       this->collectInterval = collectInterval;
         
    }
- readLine ++;
- }
+   this->currentStopIndex ++;
+   EEPROM.write(address, currentStopIndex); //Escrevendo na EPROMM o currentStopIndex23 https://www.arduino.cc/en/Tutorial/EEPROMWrite
+   address = address + 1;
+   if(address == EEPROM.length()){
+      address = 0;
+   }
+   ++ address & = EEPROM.length() -1; //impedimento da memória EEPROM estourar.
+}
+
+fclose(arq);
+ 
 
  fclose(arq);
 }
