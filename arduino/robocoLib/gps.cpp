8da38f473cb0f8dbe1141a5d4754c5ef0e6ec87f
leonardomello27 <31973297+leonardomello27@users.noreply.github.com>
2019-04-16 23:10:09 -0300

    collectRegister.cpp
    alterações realizadas dia 16/04/2019

--- arduino/robocoLib/gps.cpp
+++ arduino/robocoLib/gps.cpp
@@ -1,28 +1,35 @@
 #include <Arduino.h>
-#include "gps.h"
+#include <stdio.h>
+#include <SD.h>
+#include "collectRegister.h"
+#include "Sensor.h"
+#include <SPI.h>
 
-Motor::Motor(int8_t pin1, int8_t pin2, int8_t pinPwm){
-  this->pin1=pin1;
-  this->pin2=pin2;
-  this->pinPwm=pinPwm;
-  pinMode(pin1, OUTPUT);
-  pinMode(pin2, OUTPUT);
-  pinMode(pinPwm, OUTPUT);
-  this->stop();
+CollectRegister::CollectRegister(int8_t pinSD){
+	this->pinSD = pinSD;
 }
 
-void Motor::stop(){
-  analogWrite(pinPwm, 0);
+CollectRegister::open(char* fileName){ 
+	
+	fileName = SD.open("result.txt", FILE_WRITE);
 }
-void Motor::move(MotorDirection direction,unsigned char speed){
-  
-  if(direction == CLOCKWISE){
-     digitalWrite(pin1, HIGH);
-     digitalWrite(pin2, LOW);   
-  }else{
-     digitalWrite(pin1, LOW);
-     digitalWrite(pin2, HIGH);
-  }
-  analogWrite(pinPwm, speed);
+
+CollectRegister::write(Location* location, Sensors* sensors){
+	this->location = location;
+	this->sensors = sensors;
+	myFile.println(Sensors.getLuminosity());
+	myFile.println(',');
+	myFile.println(Sensors.getTemperature());
+	myFile.println(',');
+	myFile.println(Sensors.getAltitude());
+	myFile.println(',');
+	myFile.println(Sensors.getAtmosphericPressure());
+	myFile.println(',');
+	myFile.println(Sensors.getCO2ppm());
+	myFile.println(';');
 }
+CollectRegister::close(){
+    myFile.close();
+}
+	
 
