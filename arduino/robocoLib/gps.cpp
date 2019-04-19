a2c9053cd18e252ea9bddd53da4a20572120afd6
duaneoli <31973219+duaneoli@users.noreply.github.com>
2019-04-19 18:38:55 -0300

    atualização gps
    gps em classe corretamente

--- arduino/robocoLib/gps.cpp
+++ arduino/robocoLib/gps.cpp
@@ -1,58 +1,65 @@
 #include<Arduino.h>
+
+
 #include "gps.h"
 
 #define GPSECHO  true
-
-void Gps::testeGps(){
- Serial.begin(115200);
-  while (!Serial);
-  delay(1000);
-  Serial.println("Adafruit GPS logging start test!");
-
-  // 9600 NMEA is the default baud rate for MTK - some use 4800
-  GPS.begin(9600);
-  
-  // You can adjust which sentences to have the module emit, below
-  // Default is RMC + GGA
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // Default is 1 Hz update rate
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
-
-  // the nice thing about this code is you can have a timer0 interrupt go off
-  // every 1 millisecond, and read data from the GPS for you. that makes the
-  // loop code a heck of a lot easier!
-  useInterrupt(true);
-  delay(500);
-  Serial.print("\nSTARTING LOGGING....");
-  if (GPS.LOCUS_StartLogger())
-    Serial.println(" STARTED!");
-  else
-    Serial.println(" no response :(");
-  delay(1000);
+GPS::GPS(int8_t pinRx, int8_t pinTx){
+    this->gps = new Adafruit_GPS(new SoftwareSerial(pinRx,pinTx));
+    iniciando();
 }
-void Gps::useInterrupt(boolean v){
-    if (v) {
-    // Timer0 is already used for millis() - we'll just interrupt somewhere
-    // in the middle and call the "Compare A" function above
-    OCR0A = 0xAF;
-    TIMSK0 |= _BV(OCIE0A);
-    usingInterrupt = true;
-    } else {
-    // do not call the interrupt function COMPA anymore
-    TIMSK0 &= ~_BV(OCIE0A);
-    usingInterrupt = false;
+GPS::GPS(HardwareSerial * serial){
+      this->gps = new Adafruit_GPS(serial);
+      iniciando();
+}
+void GPS::iniciando(){
+  this->gps->begin(9600);
+  this->gps->sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  this->gps->sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  this->gps->sendCommand(PGCMD_ANTENNA);
+}
+void GPS::readGps(){
+  bool x = false;
+  char c=0;
+  while(x == false){
+    delay(10);
+    c = this->gps->read();
+    /*if (GPSECHO){ 
+      if(c=='$'){
+        Serial.println();
+      }
+      Serial.print(c);
+    }*/
+  if (this->gps->newNMEAreceived()) {
+    x = true;
+    if (!this->gps->parse(this->gps->lastNMEA())){
+		  //receber sentença correta
+      }
     }
-}
-SIGNAL(TIMER0_COMPA_vect) {
-  char c = GPS.read();
-  // if you want to debug, this is a good time to do it!
-  if (GPSECHO && c) {
-#ifdef UDR0
-    UDR0 = c;  
-    // writing direct to UDR0 is much much faster than Serial.print 
-    // but only one character can be written at a time. 
-#else
-    Serial.write(c);
-#endif
   }
+}
+
+Location* GPS::getCurrentLocation(){
+  Location* location = new Location();
+  this->readGps();
+    location->longitude = this->gps->longitudeDegrees;
+    location->latitude = this->gps->latitudeDegrees;
+    location->altitude = this->gps->altitude;
+    location->angle = this->gps->angle;
+  
+  return location; 
+
+}
+
+DataTimer* GPS::getCurrentDataTimer(){
+  DataTimer* dataTimer = new DataTimer();
+  this->readGps();
+    dataTimer->day = gps->day;
+    dataTimer->month = gps->month;
+    dataTimer->year = gps->year;
+    dataTimer->hour  = gps->hour;
+    dataTimer->minute = gps->minute;
+    dataTimer->seconds = gps->seconds;
+
+  return dataTimer;
 }
--- arduino/robocoLib/gps.h
+++ arduino/robocoLib/gps.h
@@ -1,19 +1,34 @@
-#ifdef GPS_CPP
+#ifndef GPS_CPP
 #define GPS_CPP
 #include <Arduino.h>
 #include <Adafruit_GPS.h>
-#include <SoftwareSerial.h>
 
-class GPS
-{
-    private:
-        SoftwareSerial mySerial(8, 7);
-        Adafruit_GPS* gps(&mySerial);
-        boolean usingInterrupt = false;  
-        
-    public:
-        void useInterrupt(boolean v); // Func prototype keeps Arduino 0023 happy
-        void testeGps();
+struct Location{
+    float latitude;
+    float longitude;
+    float altitude;
+    float angle;
 };
 
+struct DataTimer
+{
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t seconds;
+    uint8_t year;
+    uint8_t month;
+    uint8_t day;
+};
+
+class GPS{
+    private:
+        void iniciando();
+        void readGps();
+    public:
+        Adafruit_GPS* gps;
+        GPS(int8_t pinRx, int8_t pinTx);
+        GPS(HardwareSerial * serial);    
+        Location* getCurrentLocation();
+        DataTimer* getCurrentDataTimer();
+};
 #endif
