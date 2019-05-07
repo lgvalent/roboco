bb16b74bdb6f70edefff1737b9961ecc3e1266fa
leonardomello27 <31973297+leonardomello27@users.noreply.github.com>
2019-05-07 13:16:45 -0300

    Update collectRegister.h
    mudanças realizados dia 07/05/2019 reunião

--- arduino/robocoLib/collectRegister.h
+++ arduino/robocoLib/collectRegister.h
@@ -1,15 +1,36 @@
-#ifndef COLLECT_REGISTER_H
-#define COLLECT_REGISTER_H
-#include <gps.h>
-#include "sensor.h"
+#include <Arduino.h>
+#include <stdio.h>
+#include <SD.h>
+#include "collectRegister.h"
+#include "Sensor.h"
+#include <SPI.h>
 
-class CollectRegister{
-  private:
-  public:
-    CollectRegister(int8_t pinSD);
-	  void open(char* fileName);
-    void write(Location* location, Sensors* sensors);
-	  void close();
-    void delete();
-};
-#endif
+CollectRegister::CollectRegister(int8_t pinSD){
+	this->pinSD = pinSD;
+}
+
+CollectRegister::open(){ 
+	this->myFile = SD.open("result.txt", FILE_WRITE);
+	myfile.println("Luminosity, Temperature, Altitude, AtmosphericPressure");
+}
+
+CollectRegister::write(Location* location, Sensors* sensors){
+	this->location = location;
+	this->sensors = sensors;
+	if(myFile){
+		myFile.print(Sensors.getLuminosity());
+		myFile.print(',');
+		myFile.print(Sensors.getTemperature());
+		myFile.print(',');
+		myFile.print(Sensors.getAltitude());
+		myFile.print(',');
+		myFile.print(Sensors.getAtmosphericPressure());
+		myFile.print(',');
+		myFile.print(Sensors.getCO2ppm());
+		myFile.println(';');
+	}	
+}
+
+CollectRegister::close(){
+    myFile.close();
+}
